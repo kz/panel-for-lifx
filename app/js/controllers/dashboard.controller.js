881ae7c45923c177c2057478088644bd3a671a4b
Kelvin <hello@iamkelv.in>
2015-06-28 16:02:09 +0100

    Update controller

--- app/js/controllers/dashboard.controller.js
+++ app/js/controllers/dashboard.controller.js
@@ -13,10 +13,32 @@ function DashboardController() {
   vm.title = 'DashboardController';
 
 
-
   ////////////////
 
 
+}
+
+
+angular
+  .module('app')
+  .controller('DashboardController', DashboardController);
+
+DashboardController.$inject = ['$scope, $window'];
+
+/* @ngInject */
+function DashboardController($scope, $window) {
+  /* jshint validthis: true */
+  var vm = this;
+
+  vm.activate = activate;
+  vm.title = 'DashboardController';
+
+  activate();
+
+  ////////////////
+
+  function activate() {
+  }
 
 
 }
